f29aa5c410c2ccbc462f7a1c94c34f0494fe3a85
Chi Kei Chan <chikeichan@gmail.com>
2014-11-18 15:09:58 -0800

    2. Draw on the Grid

--- tetris.js
+++ tetris.js
@@ -5,16 +5,32 @@ tetris.drawPlayField = function(){
 	for(var row=0;row<22;row++){
 		$('#playfield').append('<tr class="'+row+'"></tr>');
 		for (var col=0;col<10;col++){
-			$('.'+row).append('<td id="'+col+'"></td>');
+			$('tr.'+row).append('<td id="'+col+'"></td>');
 		}
 	}
 }
 
+//Variable to store current coordiates
+tetris.currentCoor = [{row:1,col:1},
+											{row:1,col:2},
+											{row:2,col:1},
+											{row:2,col:2}];
 
+
+//Fill the cells
+tetris.fillCells = function(coordinates,fillColor){
+	for(var i=0;i<coordinates.length;i++){
+		var row = coordinates[i].row;
+		var col = coordinates[i].col;
+		var $coor = $('.'+row).find('#'+col);
+		$coor.attr('bgcolor',fillColor);
+	}
+}
 
 
 $(document).ready(function(){
 	
 	tetris.drawPlayField();
+	tetris.fillCells(tetris.currentCoor,'black');
 
 })
